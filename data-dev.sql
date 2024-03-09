ed659a2536d9e389784bf98cbaf0b2f395e759d1
JayexDesigns <joaquintouron@gmail.com>
2024-03-10 00:17:01 +0100

    update: added separation of sql init files for different environments

---
+++ data-dev.sql
@@ -0,0 +1,26 @@
+-- data.sql
+
+-- Create the uuid extension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Create the enum user_type
+CREATE TYPE user_type AS ENUM ('admin', 'neighbor');
+
+-- Create the table users
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(50) NOT NULL,
+	image_url VARCHAR(2048) NOT NULL,
+	balance int NOT NULL,
+	type user_type NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Insert some sample data
+INSERT INTO users (password, email, username, image_url, balance, type)
+VALUES
+    ('1234', 'joedoe@example.com', 'JoeDoe', 'https://example.com/joedoe.jpg', 0, 'admin'),
+    ('5678', 'janedoe@example.com', 'JaneDoe', 'https://example.com/janedoe.jpg', 100, 'neighbor');
