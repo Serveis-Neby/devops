093f0c08d984fd4ec6606ba432687087066fa224
JayexDesigns <joaquintouron@gmail.com>
2024-03-09 23:09:26 +0100

    update: improved user schema

--- data.sql
+++ data.sql
@@ -1,13 +1,20 @@
 -- data.sql
 
--- Crear la tabla users
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL
-);
+-- Create the uuid extension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Insertar algunos datos de ejemplo
-INSERT INTO users (username, email) VALUES
-    ('usuario1', 'usuario1@example.com'),
-    ('usuario2', 'usuario2@example.com');
+-- Create the enum user_type
+CREATE TYPE user_type AS ENUM ('admin', 'neighbor');
+
+-- Create the table users
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(50) NOT NULL,
+	image_url VARCHAR(2048) NOT NULL,
+	balance int NOT NULL,
+	type user_type NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
